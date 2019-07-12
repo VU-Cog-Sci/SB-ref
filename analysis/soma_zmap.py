1494a15f9b9e62e7218927ee6e41903bbe15bef3
verissimo <verissimo.iness@gmail.com>
2019-07-13 00:26:22 +0200

    changed to use new processed files, can decided to use smoothed data or not. also, can run for median sub

--- analysis/soma_zmap.py
+++ analysis/soma_zmap.py
@@ -15,8 +15,9 @@ import re
 import numpy as np
 import pandas as pd
 
+import nibabel as nb
+from nilearn import surface
 
-from nilearn.signal import clean
 from nistats.design_matrix import make_first_level_design_matrix
 from nistats.first_level_model import run_glm
 from nistats.contrasts import compute_contrast
@@ -36,90 +37,137 @@ else:
             analysis_params = json.load(json_file)	
     
 
-# define paths and list of files
-filepath = glob.glob(os.path.join(analysis_params['fmriprep_dir'],'sub-{sj}'.format(sj=sj),'*','func/*'))
-eventpath = glob.glob(os.path.join(analysis_params['sourcedata_dir'],'sub-{sj}'.format(sj=sj),'*','func/*'))
+# use smoothed data, maybe add this to json        
+with_smooth = True
 
-# list of functional files
-filename = [run for run in filepath if 'soma' in run and 'fsaverage' in run and run.endswith('.func.gii')]
-filename.sort()
-# list of confounds
-confounds = [run for run in filepath if 'soma' in run and run.endswith('_desc-confounds_regressors.tsv')]
-confounds.sort()
-# list of stimulus onsets
-events = [run for run in eventpath if 'soma' in run and run.endswith('events.tsv')]
-events.sort()
+if sj == 'median':
+    allsubdir = glob.glob(os.path.join(analysis_params['post_fmriprep_outdir'],'soma','sub-*/'))
+    alleventdir = glob.glob(os.path.join(analysis_params['sourcedata_dir'],'sub-*/'))
+    
+else: # if individual subject
+    allsubdir = glob.glob(os.path.join(analysis_params['post_fmriprep_outdir'],'soma','sub-{sj}'.format(sj=sj)))
+    alleventdir = glob.glob(os.path.join(analysis_params['sourcedata_dir'],'sub-{sj}'.format(sj=sj)))
+        
+allsubdir.sort()
+alleventdir.sort()
 
+onsets_allsubs = []
+durations_allsubs = []
+    
+for idx,subdir in enumerate(allsubdir): #loop over all subjects in defined list
 
-# high pass filter all runs
-# exception for these 2 subjects, TR was different
-for string in ['sub-01_ses-01', 'sub-03_ses-01']:
-    if re.search(string, filename[0]):
-        TR = 1.5
+    print('functional files from %s'%allsubdir[idx])
+    print('event files from %s'%alleventdir[idx])
+
+    # define paths and list of files
+    filepath = glob.glob(os.path.join(subdir,'*'))
+    eventpath = glob.glob(os.path.join(alleventdir[idx],'*','func/*'))
+
+    # changes depending on data used
+    if with_smooth==True:
+        # soma out path
+        soma_out = os.path.join(analysis_params['soma_outdir'],'sub-{sj}'.format(sj=sj),'run-median','smooth')
+        # list of functional files
+        filename = [run for run in filepath if 'soma' in run and 'fsaverage' in run and run.endswith('smooth5.mgz')]
     else:
-        TR = analysis_params["TR"]
+        # soma out path
+        soma_out = os.path.join(analysis_params['soma_outdir'],'sub-{sj}'.format(sj=sj),'run-median')
+        # list of functional files
+        filename = [run for run in filepath if 'soma' in run and 'fsaverage' in run and run.endswith('_sg_conf.mgz')]
 
-# soma out path
-soma_out = os.path.join(analysis_params['soma_outdir'],'sub-{sj}'.format(sj=sj),'run-median')
+    filename.sort()
 
-if not os.path.exists(soma_out): # check if path to save median run exist
-    os.makedirs(soma_out) 
-
-# savgol filter giis and confounds
-filt_gii = highpass_gii(filename,analysis_params['sg_filt_polyorder'],analysis_params['sg_filt_deriv'],
-         analysis_params['sg_filt_window_length'],soma_out)
-
-filt_conf = highpass_confounds(confounds,analysis_params['nuisance_columns'],analysis_params['sg_filt_polyorder'],analysis_params['sg_filt_deriv'],
-                   analysis_params['sg_filt_window_length'],TR,soma_out)
+    if not os.path.exists(soma_out): # check if path to save median run exist
+        os.makedirs(soma_out) 
 
 
-# regress the confounds from signal
-data = []
+    # list of stimulus onsets
+    events = [run for run in eventpath if 'soma' in run and run.endswith('events.tsv')]
+    events.sort()
 
-for idx,file in enumerate(filt_conf):
-    confs = pd.read_csv(file, sep='\t', na_values='n/a')
-    d = clean(filt_gii[idx], confounds=confs.values, standardize=False)
-    
-    data.append(d)
-
-data = np.array(data) # cleaned data
-
-# compute median soma file
-median_data = np.median(data,axis=0)
-
-# Append all events in same dataframe
-print('Loading events')
-
-all_events = []
-for _,val in enumerate(events):
-    
-    events_pd = pd.read_csv(val,sep = '\t')
-
-    new_events = []
-    
-    for ev in events_pd.iterrows():
-        row = ev[1]   
-        if row['trial_type'][0] == 'b': # if both hand/leg then add right and left events with same timings
-            new_events.append([row['onset'],row['duration'],'l'+row['trial_type'][1:]])
-            new_events.append([row['onset'],row['duration'],'r'+row['trial_type'][1:]])
+    # exception for these 2 subjects, TR was different
+    for string in ['sub-01_ses-01', 'sub-03_ses-01']:
+        if re.search(string, filename[0]):
+            TR = 1.5
         else:
-            new_events.append([row['onset'],row['duration'],row['trial_type']])
-   
-    df = pd.DataFrame(new_events, columns=['onset','duration','trial_type'])  #make sure only relevant columns present
-    all_events.append(df)
+            TR = analysis_params["TR"]
+    
+    #load processed data        
+    all_files = []
+    all_runs = np.arange(1,5) # 4 runs for soma task
 
+    for run in all_runs: # for every run (2 hemi per run)
+        run_files = [x for _,x in enumerate(filename) if 'run-'+str(run).zfill(2) in os.path.split(x)[-1]]
+
+        if not run_files:
+                print('no files for run-%s' %str(run).zfill(2))
+        else:
+            data_both = []
+            for _,hemi in enumerate(run_files):
+                data_both.append(surface.load_surf_data(hemi).T) #load surface data
+
+            data_both = np.hstack(data_both) #stack then filter
+
+            all_files.append(data_both)
+
+    # all runs stacked in same array
+    all_files = np.array(all_files)
+    
+    # compute median soma file
+    median_data = np.median(all_files,axis=0)
+    
+    if idx == 0:
+        median_sub = median_data[np.newaxis,:,:]
+    else:
+        median_sub = np.vstack((median_sub,median_data[np.newaxis,:,:]))
+        
+    # Append all events in same dataframe
+    print('Loading events')
+
+    all_events = []
+    for _,val in enumerate(events):
+
+        events_pd = pd.read_csv(val,sep = '\t')
+
+        new_events = []
+
+        for ev in events_pd.iterrows():
+            row = ev[1]   
+            if row['trial_type'][0] == 'b': # if both hand/leg then add right and left events with same timings
+                new_events.append([row['onset'],row['duration'],'l'+row['trial_type'][1:]])
+                new_events.append([row['onset'],row['duration'],'r'+row['trial_type'][1:]])
+            else:
+                new_events.append([row['onset'],row['duration'],row['trial_type']])
+
+        df = pd.DataFrame(new_events, columns=['onset','duration','trial_type'])  #make sure only relevant columns present
+        all_events.append(df)
+
+    # make median event dataframe
+    onsets = []
+    durations = []
+    for w in range(len(all_events)):
+        onsets.append(all_events[w]['onset'])
+        durations.append(all_events[w]['duration'])
+        
+    onsets_allsubs.append(np.median(np.array(onsets),axis=0)) #append average onset of all runs
+    durations_allsubs.append(np.median(np.array(durations),axis=0))
+
+
+# all subjects in one array, use this to compute contrasts
+median_data_all = np.median(median_sub,axis=0)
+events_avg = pd.DataFrame({'onset':np.median(np.array(onsets_allsubs),axis=0),'duration':np.median(np.array(durations_allsubs),axis=0),'trial_type':all_events[0]['trial_type']})
 
 # specifying the timing of fMRI frames
-frame_times = TR * (np.arange(median_data.shape[0]))
+frame_times = TR * (np.arange(median_data_all.shape[0]))
 
 # Create the design matrix, hrf model containing Glover model 
 design_matrix = make_first_level_design_matrix(frame_times,
-                                               events=all_events[0],
+                                               events=events_avg,
                                                hrf_model='glover'
                                                )
 
 # Setup and fit GLM, estimates contains the parameter estimates
-labels, estimates = run_glm(median_data, design_matrix.values)
+labels, estimates = run_glm(median_data_all, design_matrix.values)
 
 print('Computing simple contrasts')
 
@@ -238,11 +286,6 @@ for i,part in enumerate(face):
     np.save(zscore_file,z_map)        
 
 print('Success!')
-
-
-
-
-
 
 
 
