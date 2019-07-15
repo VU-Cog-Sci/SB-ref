fb7670b3643305f072d45157eca8567735427c05
verissimo <verissimo.iness@gmail.com>
2019-07-15 14:28:46 +0200

    small fix

--- analysis/visualize_cortex.py
+++ analysis/visualize_cortex.py
@@ -51,132 +51,131 @@ if not os.path.exists(flatmap_out): # check if path for outputs exist
 
 ## PRF ##
 
-if os.path.isdir(median_path):
 
-  if sj=='median':
-        
-    estimates = median_pRFestimates(analysis_params['pRF_outdir'],with_smooth=bool(strtobool(with_smooth)))
-    xx = estimates['x']
-    yy = estimates['y']
-    rsq = estimates['r2']
-    size = estimates['size']
-    beta = estimates['betas']
-    baseline = estimates['baseline']
+if sj=='median':
+      
+  estimates = median_pRFestimates(analysis_params['pRF_outdir'],with_smooth=bool(strtobool(with_smooth)))
+  xx = estimates['x']
+  yy = estimates['y']
+  rsq = estimates['r2']
+  size = estimates['size']
+  beta = estimates['betas']
+  baseline = estimates['baseline']
 
-  else:
-    estimates_list = [x for x in os.listdir(median_path) if x.endswith('estimates.npz') ]
-    estimates_list.sort() #sort to make sure pRFs not flipped
-    
-    estimates = []
-    for _,val in enumerate(estimates_list) :
-        estimates.append(np.load(os.path.join(median_path, val))) #save both hemisphere estimates in same array
+else:
+  estimates_list = [x for x in os.listdir(median_path) if x.endswith('estimates.npz') ]
+  estimates_list.sort() #sort to make sure pRFs not flipped
+  
+  estimates = []
+  for _,val in enumerate(estimates_list) :
+      estimates.append(np.load(os.path.join(median_path, val))) #save both hemisphere estimates in same array
 
-    # concatenate r2 and parameteres, to later visualize whole brain
-    rsq = np.concatenate((estimates[0]['r2'],estimates[1]['r2']))
+  # concatenate r2 and parameteres, to later visualize whole brain
+  rsq = np.concatenate((estimates[0]['r2'],estimates[1]['r2']))
 
-    xx = np.concatenate((estimates[0]['x'],estimates[1]['x']))
-    yy = np.concatenate((estimates[0]['y'],estimates[1]['y']))
+  xx = np.concatenate((estimates[0]['x'],estimates[1]['x']))
+  yy = np.concatenate((estimates[0]['y'],estimates[1]['y']))
 
-    size = np.concatenate((estimates[0]['size'],estimates[1]['size']))
-    baseline = np.concatenate((estimates[0]['baseline'],estimates[1]['baseline']))
-    beta = np.concatenate((estimates[0]['betas'],estimates[1]['betas']))
+  size = np.concatenate((estimates[0]['size'],estimates[1]['size']))
+  baseline = np.concatenate((estimates[0]['baseline'],estimates[1]['baseline']))
+  beta = np.concatenate((estimates[0]['betas'],estimates[1]['betas']))
 
-  # now construct polar angle and eccentricity values
-  # grid fit function gives out [x,y,size,baseline,betas]
-  rsq_threshold = analysis_params['rsq_threshold']
+# now construct polar angle and eccentricity values
+# grid fit function gives out [x,y,size,baseline,betas]
+rsq_threshold = analysis_params['rsq_threshold']
 
-  complex_location = xx + yy * 1j
-  polar_angle = np.angle(complex_location)
-  eccentricity = np.abs(complex_location)
+complex_location = xx + yy * 1j
+polar_angle = np.angle(complex_location)
+eccentricity = np.abs(complex_location)
 
-  # normalize polar angles to have values in circle between 0 and 1
-  polar_ang_norm = (polar_angle + np.pi) / (np.pi * 2.0)
+# normalize polar angles to have values in circle between 0 and 1
+polar_ang_norm = (polar_angle + np.pi) / (np.pi * 2.0)
 
-  # use "resto da divisão" so that 1 == 0 (because they overlapp in circle)
-  # why have an offset?
-  angle_offset = 0.1
-  polar_ang_norm = np.fmod(polar_ang_norm+angle_offset, 1.0)
+# use "resto da divisão" so that 1 == 0 (because they overlapp in circle)
+# why have an offset?
+angle_offset = 0.1
+polar_ang_norm = np.fmod(polar_ang_norm+angle_offset, 1.0)
 
-  # convert angles to colors, using correlations as weights
-  hsv = np.zeros(list(polar_ang_norm.shape) + [3])
-  hsv[..., 0] = polar_ang_norm # different hue value for each angle
-  hsv[..., 1] = (rsq > rsq_threshold).astype(float)#  np.ones_like(rsq) # saturation weighted by rsq
-  hsv[..., 2] = (rsq > rsq_threshold).astype(float) # value weighted by rsq
+# convert angles to colors, using correlations as weights
+hsv = np.zeros(list(polar_ang_norm.shape) + [3])
+hsv[..., 0] = polar_ang_norm # different hue value for each angle
+hsv[..., 1] = (rsq > rsq_threshold).astype(float)#  np.ones_like(rsq) # saturation weighted by rsq
+hsv[..., 2] = (rsq > rsq_threshold).astype(float) # value weighted by rsq
 
-  # convert hsv values of np array to rgb values (values assumed to be in range [0, 1])
-  rgb = colors.hsv_to_rgb(hsv)
+# convert hsv values of np array to rgb values (values assumed to be in range [0, 1])
+rgb = colors.hsv_to_rgb(hsv)
 
-  # define alpha channel - which specifies the opacity for a color
-  # 0 = transparent = values with rsq below thresh and 1 = opaque = values above thresh
-  alpha_mask = (rsq <= rsq_threshold).T #why transpose? because of orientation of pycortex volume?
-  alpha = np.ones(alpha_mask.shape)
-  alpha[alpha_mask] = 0
-
-
-  # define Vertex images
-
-  #contains RGBA colors for each voxel in a volumetric dataset
-  # vertex for polar angles
-  images['polar'] = cortex.VertexRGB(rgb[..., 0].T, 
-                                   rgb[..., 1].T, 
-                                   rgb[..., 2].T, 
-                                   subject='fsaverage', alpha=alpha)
-
-  # vertex for ecc
-  images['ecc'] = cortex.Vertex2D(eccentricity.T, alpha*10, 'fsaverage',
-                             vmin=0, vmax=10,
-                             vmin2=0, vmax2=1.0, cmap='BROYG_2D')
-  #images['ecc'] = cortex.Vertex2D(eccentricity.T, rsq.T, 'fsaverage',
-  #                           vmin=0, vmax=10,
-  #                           vmin2=rsq_threshold, vmax2=1.0, cmap='BROYG_2D')
-
-  # vertex for size
-  images['size'] = cortex.dataset.Vertex2D(size.T, alpha*10, 'fsaverage',
-                             vmin=0, vmax=10,
-                             vmin2=0, vmax2=1.0, cmap='BROYG_2D')
-  #images['size'] = cortex.Vertex2D(size.T, rsq.T, 'fsaverage',
-  #                           vmin=0, vmax=10,
-  #                           vmin2=rsq_threshold, vmax2=1.0, cmap='BROYG_2D')
-
-  # vertex for betas (amplitude?)
-  images['beta'] = cortex.Vertex2D(beta.T, rsq.T, 'fsaverage',
-                             vmin=-2.5, vmax=2.5,
-                             vmin2=rsq_threshold, vmax2=1.0, cmap='RdBu_r_alpha')
-
-  # vertex for baseline
-  images['baseline'] = cortex.Vertex2D(baseline.T, rsq.T, 'fsaverage',
-                             vmin=-1, vmax=1,
-                             vmin2=rsq_threshold, vmax2=1.0, cmap='RdBu_r_alpha')
-
-  # vertex for rsq
-  images['rsq'] = cortex.Vertex2D(rsq.T, alpha, 'fsaverage',
-                             vmin=0, vmax=1.0,
-                             vmin2=0, vmax2=1.0, cmap='Reds_cov')
-  #images['rsq'] = cortex.dataset.Vertex(rsq.T, 'fsaverage',
-  #                     vmin=0, vmax=np.max(rsq), cmap='Reds')
+# define alpha channel - which specifies the opacity for a color
+# 0 = transparent = values with rsq below thresh and 1 = opaque = values above thresh
+alpha_mask = (rsq <= rsq_threshold).T #why transpose? because of orientation of pycortex volume?
+alpha = np.ones(alpha_mask.shape)
+alpha[alpha_mask] = 0
 
 
-  # create flatmaps for different parameters and save png
+# define Vertex images
 
-  # Save this flatmap
-  filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-polar_angle.png' %rsq_threshold)
-  print('saving %s' %filename)
-  _ = cortex.quickflat.make_png(filename, images['polar'], recache=True,with_colorbar=False,with_curvature=True,with_sulci=True)
+#contains RGBA colors for each voxel in a volumetric dataset
+# vertex for polar angles
+images['polar'] = cortex.VertexRGB(rgb[..., 0].T, 
+                                 rgb[..., 1].T, 
+                                 rgb[..., 2].T, 
+                                 subject='fsaverage', alpha=alpha)
 
-  # Save this flatmap
-  filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-eccentricity.png' %rsq_threshold)
-  print('saving %s' %filename)
-  _ = cortex.quickflat.make_png(filename, images['ecc'], recache=True,with_colorbar=True,with_curvature=True,with_sulci=True)
+# vertex for ecc
+images['ecc'] = cortex.Vertex2D(eccentricity.T, alpha*10, 'fsaverage',
+                           vmin=0, vmax=10,
+                           vmin2=0, vmax2=1.0, cmap='BROYG_2D')
+#images['ecc'] = cortex.Vertex2D(eccentricity.T, rsq.T, 'fsaverage',
+#                           vmin=0, vmax=10,
+#                           vmin2=rsq_threshold, vmax2=1.0, cmap='BROYG_2D')
 
-  # Save this flatmap
-  filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-size.png' %rsq_threshold)
-  print('saving %s' %filename)
-  _ = cortex.quickflat.make_png(filename, images['size'], recache=True,with_colorbar=True,with_curvature=True,with_sulci=True)
+# vertex for size
+images['size'] = cortex.dataset.Vertex2D(size.T, alpha*10, 'fsaverage',
+                           vmin=0, vmax=10,
+                           vmin2=0, vmax2=1.0, cmap='BROYG_2D')
+#images['size'] = cortex.Vertex2D(size.T, rsq.T, 'fsaverage',
+#                           vmin=0, vmax=10,
+#                           vmin2=rsq_threshold, vmax2=1.0, cmap='BROYG_2D')
 
-  # Save this flatmap
-  filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-rsquared.png' %rsq_threshold)
-  print('saving %s' %filename)
-  _ = cortex.quickflat.make_png(filename, images['rsq'], recache=True,with_colorbar=True,with_curvature=True,with_sulci=True)
+# vertex for betas (amplitude?)
+images['beta'] = cortex.Vertex2D(beta.T, rsq.T, 'fsaverage',
+                           vmin=-2.5, vmax=2.5,
+                           vmin2=rsq_threshold, vmax2=1.0, cmap='RdBu_r_alpha')
+
+# vertex for baseline
+images['baseline'] = cortex.Vertex2D(baseline.T, rsq.T, 'fsaverage',
+                           vmin=-1, vmax=1,
+                           vmin2=rsq_threshold, vmax2=1.0, cmap='RdBu_r_alpha')
+
+# vertex for rsq
+images['rsq'] = cortex.Vertex2D(rsq.T, alpha, 'fsaverage',
+                           vmin=0, vmax=1.0,
+                           vmin2=0, vmax2=1.0, cmap='Reds_cov')
+#images['rsq'] = cortex.dataset.Vertex(rsq.T, 'fsaverage',
+#                     vmin=0, vmax=np.max(rsq), cmap='Reds')
+
+
+# create flatmaps for different parameters and save png
+
+# Save this flatmap
+filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-polar_angle.png' %rsq_threshold)
+print('saving %s' %filename)
+_ = cortex.quickflat.make_png(filename, images['polar'], recache=True,with_colorbar=False,with_curvature=True,with_sulci=True)
+
+# Save this flatmap
+filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-eccentricity.png' %rsq_threshold)
+print('saving %s' %filename)
+_ = cortex.quickflat.make_png(filename, images['ecc'], recache=True,with_colorbar=True,with_curvature=True,with_sulci=True)
+
+# Save this flatmap
+filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-size.png' %rsq_threshold)
+print('saving %s' %filename)
+_ = cortex.quickflat.make_png(filename, images['size'], recache=True,with_colorbar=True,with_curvature=True,with_sulci=True)
+
+# Save this flatmap
+filename = os.path.join(flatmap_out,'flatmap_space-fsaverage_rsq-%0.2f_type-rsquared.png' %rsq_threshold)
+print('saving %s' %filename)
+_ = cortex.quickflat.make_png(filename, images['rsq'], recache=True,with_colorbar=True,with_curvature=True,with_sulci=True)
 
 
 ## SOMATOTOPY ##
