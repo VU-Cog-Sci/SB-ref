37aae3a8ccafcc2c46a4638ac757737c56a88f01
verissimo <verissimo.iness@gmail.com>
2019-08-23 13:51:46 +0200

    fitting data as type float, to see if faster

--- analysis/prf_fit_FN.py
+++ analysis/prf_fit_FN.py
@@ -157,8 +157,9 @@ class FN_fit(object):
             # numpy implementation is slower?
             dm = np.vstack([np.ones_like(self.predictions[:,prediction_num]),self.predictions[:,prediction_num]]).T
             (intercept, slope), residual, _, _ = sp.linalg.lstsq(dm, self.data.T, check_finite=False) #  , lapack_driver='gelsy')
-            
+
             if residual.any()==True: #if residual not empty
+            
                 rsqs = ((1 - residual / (self.n_timepoints * self.data_var)))
 
                 improved_fits = rsqs > self.gridsearch_r2
