2312d07746ebe7f24aca8726685ae4f268780d1f
verissimo <verissimo.iness@gmail.com>
2019-11-04 14:52:07 +0100

    both timeseries in same, colored axis too

--- analysis/make_soma_plots.py
+++ analysis/make_soma_plots.py
@@ -240,6 +240,50 @@ for i in range(len(vertex)):
     fig.savefig(os.path.join(figure_out,'soma_singvoxfit_timeseries_%s.svg'%task[i]), dpi=100,bbox_inches = 'tight')
 
 
+# now combine tasks in both (to have option of different plots)
+fig, axis = plt.subplots(1,figsize=(15,7.5),dpi=100)
+task = ['face','upper_limb']
+blue_color = ['#004759','#00a7d1']
+data_color = ['#262626','#8a8a8a']
+for i in range(len(task)):
+    
+    betas_conv = np.linalg.lstsq(design_matrix.values, data[vertex[i]])[0]
+    model_sig = design_matrix.values.dot(betas_conv)
+    
+    mse = np.mean((model_sig - data[vertex[i]]) ** 2) # calculate mean of squared residuals
+    r2 = pearsonr(model_sig, data[vertex[i]])[0] ** 2 # and the rsq
+    
+    # legend labels for data
+    dlabel = 'face' if task[i]=='face' else 'hand'
+    
+    # plot data with model
+    axis.plot(time_sec,model_sig,c=blue_color[i],lw=3,label='model',zorder=1)
+    axis.scatter(time_sec,data[vertex[i]], marker='.',c=data_color[i],label=dlabel)
+    axis.set_xlabel('Time (s)',fontsize=18)
+    axis.set_ylabel('BOLD signal change (%)',fontsize=18)
+    axis.tick_params(axis='both', labelsize=14)
+    axis.set_xlim(0,len(data[vertex[i]])*TR)
+    #plt.title('voxel %d (%s) , MSE = %.3f, rsq = %.3f' %(vertex[i],task[i],mse,r2))
+    
+    # plot axis vertical bar on background to indicate stimulus display time
+    stim_onset = []
+    for w in range(len(events_avg)):
+        if events_avg['trial_type'][w] in analysis_params['all_contrasts'][task[i]]:
+            stim_onset.append(events_avg['onset'][w])
+    stim_onset = list(set(stim_onset)); stim_onset.sort()  # to remove duplicate values (both hands situation)
+            
+    ax_count = 0
+    for h in range(6):
+        incr = 3 if task[i]=='face' else 4 # increment for vertical bar (to plot it from index 0 to index 4)
+        plt.axvspan(stim_onset[ax_count], stim_onset[ax_count+3]+2.25, facecolor=blue_color[i], alpha=0.1)
+        ax_count += 4 if task[i]=='face' else 5
+
+handles,labels = axis.axes.get_legend_handles_labels()
+axis.legend([handles[0],handles[2],handles[1],handles[3]],
+            [labels[0],labels[2],labels[1],labels[3]],loc='upper left')  # doing this to guarantee that legend is how I want it   
+fig.savefig(os.path.join(figure_out,'soma_singvoxfit_timeseries_%s.svg'%str(task)), dpi=100,bbox_inches = 'tight')
+
+
 
 
 
