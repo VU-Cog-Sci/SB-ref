4643bdfd1d2fc65e26fd269a0270e5fb07723d5c
verissimo <verissimo.iness@gmail.com>
2020-03-06 00:10:23 +0100

    runs for all subs concatenated, still incomplete though

--- analysis/plot_VFcoverage_ECCdistr.py
+++ analysis/plot_VFcoverage_ECCdistr.py
@@ -11,8 +11,6 @@ import numpy as np
 from utils import *
 
 import matplotlib.pyplot as plt
-from nilearn import surface
-from distutils.util import strtobool
 
 import cortex
 import seaborn as sns
@@ -27,6 +25,8 @@ import random
 
 from matplotlib.lines import Line2D
 
+import itertools
+
 
 # define participant number and open json parameter file
 if len(sys.argv)<2: 
@@ -39,6 +39,9 @@ else:
     with open('analysis_params.json','r') as json_file: 
             analysis_params = json.load(json_file)  
 
+
+# set threshold for plotting
+rsq_threshold=0.17
 
 # fit model to use (gauss or css)
 fit_model = 'css' #analysis_params["fit_model"]
@@ -57,7 +60,26 @@ if not os.path.exists(figure_out): # check if path to save figures exists
 print('saving figures in %s'%figure_out)
 
 
-if sj != 'all': # inidividual subjects
+if sj == 'all':
+    estimates = append_pRFestimates(analysis_params['pRF_outdir'],
+                                        model=fit_model,iterative=iterative_fit,exclude_subs=['sub-07'],total_chunks=total_chunks)
+    
+    print('appended estimates for %s excluded %s'%(str(estimates['subs']),str(estimates['exclude_subs'])))
+    
+    xx = np.array(estimates['x'])
+    yy = np.array(estimates['y'])
+       
+    size = np.array(estimates['size'])
+    
+    beta = np.array(estimates['betas'])
+    baseline = np.array(estimates['baseline'])
+    
+    ns = np.array(estimates['ns'])
+
+    rsq = np.array(estimates['r2'])
+    
+else:
+    print('loading estimates for sub-%s'%sj)
     
     median_path = os.path.join(analysis_params['pRF_outdir'],'sub-{sj}'.format(sj=sj),'run-median','chunks_'+str(total_chunks))
     
@@ -81,53 +103,60 @@ if sj != 'all': # inidividual subjects
         estimates.append(np.load(os.path.join(median_path, val))) #save both hemisphere estimates in same array
 
 
-    xx = np.concatenate((estimates[0]['x'],estimates[1]['x']))
-    yy = np.concatenate((estimates[0]['y'],estimates[1]['y']))
+    xx = np.concatenate((estimates[0]['x'],estimates[1]['x'])); xx = xx[np.newaxis,:]
+    yy = np.concatenate((estimates[0]['y'],estimates[1]['y'])); yy = yy[np.newaxis,:]
        
-    size = np.concatenate((estimates[0]['size'],estimates[1]['size']))
+    size = np.concatenate((estimates[0]['size'],estimates[1]['size'])); size = size[np.newaxis,:]
     
-    beta = np.concatenate((estimates[0]['betas'],estimates[1]['betas']))
-    baseline = np.concatenate((estimates[0]['baseline'],estimates[1]['baseline']))
+    beta = np.concatenate((estimates[0]['betas'],estimates[1]['betas'])); beta = beta[np.newaxis,:]
+    baseline = np.concatenate((estimates[0]['baseline'],estimates[1]['baseline'])); baseline = baseline[np.newaxis,:]
     
     if fit_model =='css': 
-        ns = np.concatenate((estimates[0]['ns'],estimates[1]['ns'])) # exponent of css
+        ns = np.concatenate((estimates[0]['ns'],estimates[1]['ns'])); ns = ns[np.newaxis,:] # exponent of css
     else: #if gauss
-        ns = np.ones(xx.shape)
+        ns = np.ones(xx.shape); ns = ns[np.newaxis,:]
 
-    rsq = np.concatenate((estimates[0]['r2'],estimates[1]['r2']))
+    rsq = np.concatenate((estimates[0]['r2'],estimates[1]['r2'])); rsq = rsq[np.newaxis,:]
+    
 
+masked_xx = []
+masked_yy = []
+masked_size = []
+masked_beta = []
+masked_baseline = []
+masked_rsq = []
+masked_ns = []
+masked_polar_angle = []
+masked_eccentricity = []
 
-# set limits for xx and yy, forcing it to be within the screen boundaries
-if sj in ['02','11','12','13']: # linux computer has different res
+for w in range(xx.shape[0]): # loop once if one subject, or for all subjects when sj 'all'
+    
+    subject = estimates['subs'][w] if sj=='all' else 'sub-'+sj 
+    # set limits for xx and yy, forcing it to be within the screen boundaries
+    if subject in ['sub-02','sub-11','sub-12','sub-13']: # linux computer has different res
+        vert_lim_dva = (analysis_params['screenRes_HD'][-1]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes_HD'][0])
+        hor_lim_dva = (analysis_params['screenRes_HD'][0]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes_HD'][0])
+    else:    
+        vert_lim_dva = (analysis_params['screenRes'][-1]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes'][0])
+        hor_lim_dva = (analysis_params['screenRes'][0]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes'][0])
 
-    vert_lim_dva = (analysis_params['screenRes_HD'][-1]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes_HD'][0])
-    hor_lim_dva = (analysis_params['screenRes_HD'][0]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes_HD'][0])
+    # make new variables that are masked (center of RF within screen limits and only positive pRFs)
+    # also max size of RF is 10 dva
+    print('masking variables to be within screen and only show positive RF for sub %s'%subject)
+    new_estimates = mask_estimates(xx[w],yy[w],size[w],beta[w],baseline[w],rsq[w],vert_lim_dva,hor_lim_dva,ns=ns[w])
 
-
-else:    
-    vert_lim_dva = (analysis_params['screenRes'][-1]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes'][0])
-    hor_lim_dva = (analysis_params['screenRes'][0]/2) * dva_per_pix(analysis_params['screen_width'],analysis_params['screen_distance'],analysis_params['screenRes'][0])
-
-# make new variables that are masked (center of RF within screen limits and only positive pRFs)
-# also max size of RF is 10 dva
-print('masking variables to be within screen and only show positive RF')
-new_estimates = mask_estimates(xx,yy,size,beta,baseline,rsq,vert_lim_dva,hor_lim_dva,ns=ns)
-
-masked_xx = new_estimates['x']
-masked_yy = new_estimates['y']
-masked_size = new_estimates['size']
-masked_beta = new_estimates['beta']
-masked_baseline = new_estimates['baseline']
-masked_rsq = new_estimates['rsq']
-masked_ns = new_estimates['ns']
-
-# set threshold for plotting
-rsq_threshold=0.1
-
-# now construct polar angle and eccentricity values
-complex_location = masked_xx + masked_yy * 1j
-masked_polar_angle = np.angle(complex_location)
-masked_eccentricity = np.abs(complex_location)
+    masked_xx.append(new_estimates['x'])
+    masked_yy.append(new_estimates['y'])
+    masked_size.append(new_estimates['size'])
+    masked_beta.append(new_estimates['beta'])
+    masked_baseline.append(new_estimates['baseline'])
+    masked_rsq.append(new_estimates['rsq'])
+    masked_ns.append(new_estimates['ns'])
+    
+    # now construct polar angle and eccentricity values
+    complex_location = new_estimates['x'] + new_estimates['y'] * 1j
+    masked_polar_angle.append(np.angle(complex_location))
+    masked_eccentricity.append(np.abs(complex_location))
 
 
 # get vertices for subject fsaverage
@@ -154,36 +183,46 @@ left_index = cortex.db.get_surfinfo('fsaverage').left.shape[0]
 
 # coverage plots - full VF 
 
-for idx,roi in enumerate(ROIs):#enumerate(['V1']): #enumerate(ROIs):
+for idx,roi in enumerate(ROIs): #enumerate(['V1'])
     
      # get roi indices for each hemisphere
     left_roi_verts = roi_verts[roi][roi_verts[roi]<left_index]
     right_roi_verts = roi_verts[roi][roi_verts[roi]>=left_index]
 
-    # LEFT HEMI
-    left_xx = masked_xx[left_roi_verts]
-    left_yy = masked_yy[left_roi_verts]
-    left_rsq = masked_rsq[left_roi_verts]
-    
-    # now construct polar angle 
-    left_complex_rf = left_xx + left_yy * 1j
-    left_pa = np.angle(left_complex_rf)
+    left_xx_4plot = []
+    left_yy_4plot = []
+    left_pa_4plot = []
 
-    # RIGHT HEMI
-    right_xx = masked_xx[right_roi_verts]
-    right_yy = masked_yy[right_roi_verts]
-    right_rsq = masked_rsq[right_roi_verts]
+    right_xx_4plot = []
+    right_yy_4plot = []
+    right_pa_4plot = []
     
-    # now construct polar angle 
-    right_complex_rf = right_xx + right_yy * 1j
-    right_pa = np.angle(right_complex_rf)
     
+    for w in range(xx.shape[0]): # loop once if one subject, or for all subjects when sj 'all'
+        # LEFT HEMI
+        left_xx = masked_xx[w][left_roi_verts]
+        left_yy = masked_yy[w][left_roi_verts]
+        left_rsq = masked_rsq[w][left_roi_verts] 
+        left_pa = masked_polar_angle[w][left_roi_verts]
+        
+        left_xx_4plot.append(left_xx[left_rsq>rsq_threshold]) 
+        left_yy_4plot.append(left_yy[left_rsq>rsq_threshold]) 
+        left_pa_4plot.append(left_pa[left_rsq>rsq_threshold]) 
+
+        # RIGHT HEMI
+        right_xx = masked_xx[w][right_roi_verts]
+        right_yy = masked_yy[w][right_roi_verts]
+        right_rsq = masked_rsq[w][right_roi_verts] 
+        right_pa = masked_polar_angle[w][right_roi_verts]
+        
+        right_xx_4plot.append(right_xx[right_rsq>rsq_threshold]) 
+        right_yy_4plot.append(right_yy[right_rsq>rsq_threshold]) 
+        right_pa_4plot.append(right_pa[right_rsq>rsq_threshold]) 
+        
     f, ss = plt.subplots(1, 1, figsize=(12, 12), sharey=True)
 
-    ss.hexbin(left_xx[left_rsq>rsq_threshold], 
-              left_yy[left_rsq>rsq_threshold],
-              #C=left_rsq[left_rsq>rsq_threshold],
-              #reduce_C_function=np.sum,
+    ss.hexbin(np.hstack(left_xx_4plot), 
+              np.hstack(left_yy_4plot),
               gridsize=30, 
               cmap='Greens',
               extent= np.array([-1, 1, -1, 1]) * hor_lim_dva, #np.array([-hor_lim_dva,hor_lim_dva,-vert_lim_dva,vert_lim_dva]),
@@ -192,10 +231,8 @@ for idx,roi in enumerate(ROIs):#enumerate(['V1']): #enumerate(ROIs):
               edgecolors='black',
               alpha=1)
 
-    ss.hexbin(right_xx[right_rsq>rsq_threshold], 
-              right_yy[right_rsq>rsq_threshold],
-              #C=right_rsq[right_rsq>rsq_threshold],
-              #reduce_C_function=np.sum,
+    ss.hexbin(np.hstack(right_xx_4plot), 
+              np.hstack(right_yy_4plot),
               gridsize=30, 
               cmap='Reds', #'YlOrRd_r',
               extent= np.array([-1, 1, -1, 1]) * hor_lim_dva, #np.array([-hor_lim_dva,hor_lim_dva,-vert_lim_dva,vert_lim_dva]),
@@ -203,7 +240,7 @@ for idx,roi in enumerate(ROIs):#enumerate(['V1']): #enumerate(ROIs):
               linewidths=0.0625,
               edgecolors='black',
               alpha=0.5)
-
+    
     ss.set_title('Visual field coverage for %s'%roi)
     ss.set_xlabel('Horizontal visual position [dva]')
     ss.set_ylabel('Vertical visual position [dva]')
@@ -218,12 +255,6 @@ for idx,roi in enumerate(ROIs):#enumerate(['V1']): #enumerate(ROIs):
 
     plt.legend(custom_lines, ['LH', 'RH'])
 
-    #sns.despine(f, offset=10)
-
-    # Create a Rectangle patch
-    #rect = patches.Rectangle((-hor_lim_dva,-vert_lim_dva),hor_lim_dva*2,vert_lim_dva*2,linewidth=1,linestyle='--',edgecolor='w',facecolor='none',zorder=10)
-    # Add the patch to the Axes
-    #ss.add_patch(rect) 
     plt.savefig(os.path.join(figure_out,'VF_coverage_ROI-%s_hemi-both.svg'%roi),dpi=100)
     
     f, ss = plt.subplots(1, 1, figsize=(12, 12), sharey=True)
@@ -233,30 +264,41 @@ for idx,roi in enumerate(ROIs):#enumerate(['V1']): #enumerate(ROIs):
     plt.legend()
     plt.title('Histogram of PA distribution for %s'%roi)
     plt.savefig(os.path.join(figure_out,'PA_histogram_ROI-%s_hemi-both.svg'%roi),dpi=100)
+    
 
- 
 # Check ECC distributions
 
 # max eccentricity within screen dva
 max_ecc = np.sqrt(vert_lim_dva**2 + hor_lim_dva**2)
 
-ks_roi_list = [['V1','V2'],['V1','V3'],['V2','V3'],['V1','sPCS'],['V1','iPCS'],['sPCS','iPCS']]
+# make list of roi pairs, to compare distributions
+ks_roi_list = np.array([pair for pair in itertools.combinations(ROIs,2)])
 
 for _,rois_ks in enumerate(ks_roi_list):
     
     # plot Empirical distribution functions
     #rois_ks = np.array(['V1','V2']) # rois to compare ecc distribution
     
-    new_rsq1 = masked_rsq[roi_verts[rois_ks[0]]]
-    new_ecc1 = masked_eccentricity[roi_verts[rois_ks[0]]]
-    indices4plot_1 = np.where((new_rsq1>rsq_threshold) & (np.logical_not(np.isnan(new_ecc1))))
+    ecc1_4plot = []
+    ecc2_4plot = []
     
-    new_rsq2 = masked_rsq[roi_verts[rois_ks[1]]]
-    new_ecc2 = masked_eccentricity[roi_verts[rois_ks[1]]]
-    indices4plot_2 = np.where((new_rsq2>rsq_threshold) & (np.logical_not(np.isnan(new_ecc2))))
+    for w in range(xx.shape[0]): # loop once if one subject, or for all subjects when sj 'all'
+        
+        new_rsq1 = masked_rsq[w][roi_verts[rois_ks[0]]]
+        new_ecc1 = masked_eccentricity[w][roi_verts[rois_ks[0]]]
+        indices4plot_1 = np.where((new_rsq1>rsq_threshold) & (np.logical_not(np.isnan(new_ecc1))))
+        
+        ecc1_4plot.append(np.sort(new_ecc1[indices4plot_1]))
+        
+        new_rsq2 = masked_rsq[w][roi_verts[rois_ks[1]]]
+        new_ecc2 = masked_eccentricity[w][roi_verts[rois_ks[1]]]
+        indices4plot_2 = np.where((new_rsq2>rsq_threshold) & (np.logical_not(np.isnan(new_ecc2))))
+        
+        ecc2_4plot.append(np.sort(new_ecc2[indices4plot_2]))
+        
 
-    ecc_roi1 = np.sort(new_ecc1[indices4plot_1])
-    ecc_roi2 = np.sort(new_ecc2[indices4plot_2])
+    ecc_roi1 = np.sort(np.hstack(ecc1_4plot))
+    ecc_roi2 = np.sort(np.hstack(ecc2_4plot))
                               
     fig, ax = plt.subplots(1, 2, figsize=(16, 8), sharey=True)
 
@@ -291,14 +333,18 @@ fig, ax = plt.subplots(1, 1, figsize=(16, 8), sharey=True)
 
 for _,rois_ks in enumerate(ROIs):
     
-    # plot Empirical distribution functions
-    #rois_ks = np.array(['V1','V2']) # rois to compare ecc distribution
-    new_rsq = masked_rsq[roi_verts[rois_ks]]
-    new_ecc = masked_eccentricity[roi_verts[rois_ks]]
-    indices4plot = np.where((new_rsq>rsq_threshold) & (np.logical_not(np.isnan(new_ecc))))
+    ecc_4plot = []
+    
+    for w in range(xx.shape[0]): # loop once if one subject, or for all subjects when sj 'all'
+        
+        new_rsq = masked_rsq[w][roi_verts[rois_ks]]
+        new_ecc = masked_eccentricity[w][roi_verts[rois_ks]]
+        indices4plot = np.where((new_rsq>rsq_threshold) & (np.logical_not(np.isnan(new_ecc))))
+        
+        ecc_4plot.append(np.sort(new_ecc[indices4plot]))
 
-    ecc_roi = np.sort(new_ecc[indices4plot])
-
+    ecc_roi = np.sort(np.hstack(ecc_4plot))
+    
     n1 = np.arange(1,len(ecc_roi)+1) / np.float(len(ecc_roi))
     ax.step(ecc_roi,n1,label=rois_ks)
 
@@ -317,13 +363,17 @@ fig, ax = plt.subplots(1, 1, figsize=(16, 8), sharey=True)
 
 for _,rois_ks in enumerate(ROIs):
     
-    # plot Empirical distribution functions
-    #rois_ks = np.array(['V1','V2']) # rois to compare ecc distribution
-    new_rsq = masked_rsq[roi_verts[rois_ks]]
-    new_ecc = masked_eccentricity[roi_verts[rois_ks]]
-    indices4plot = np.where((new_rsq>rsq_threshold) & (np.logical_not(np.isnan(new_ecc))))
+    ecc_4plot = []
+    
+    for w in range(xx.shape[0]): # loop once if one subject, or for all subjects when sj 'all'
+        
+        new_rsq = masked_rsq[w][roi_verts[rois_ks]]
+        new_ecc = masked_eccentricity[w][roi_verts[rois_ks]]
+        indices4plot = np.where((new_rsq>rsq_threshold) & (np.logical_not(np.isnan(new_ecc))))
+        
+        ecc_4plot.append(np.sort(new_ecc[indices4plot]))
 
-    ecc_roi = np.sort(new_ecc[indices4plot])
+    ecc_roi = np.sort(np.hstack(ecc_4plot))
     
     ax = sns.kdeplot(ecc_roi,label=rois_ks)
     ax.set_title('Kernel density')
@@ -334,3 +384,6 @@ for _,rois_ks in enumerate(ROIs):
     ax.legend()
 
 plt.savefig(os.path.join(figure_out,'KDE_ROI-all.svg'),dpi=100)
+
+
+
